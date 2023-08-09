950f59b007d42662c8adf7af1f7526beab501026
suyong <emuext@gmail.com>
2023-08-09 22:16:48 +0900

    Spring_Cloud_Studyd_order-service

---
+++ src/main/java/com/example/orderservice/service/OrderServiceImpl.java
@@ -0,0 +1,54 @@
+package com.example.orderservice.service;
+
+import com.example.orderservice.dto.OrderDto;
+import com.example.orderservice.jpa.OrderEntity;
+import com.example.orderservice.jpa.OrderRepository;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Data
+@Slf4j
+@Service
+public class OrderServiceImpl implements OrderService {
+
+    OrderRepository orderRepository;
+
+    @Autowired
+    public OrderServiceImpl(OrderRepository orderRepository) {
+        this.orderRepository = orderRepository;
+    }
+
+    @Override
+    public OrderDto createOrder(OrderDto orderDto) {
+        orderDto.setOrderId(UUID.randomUUID().toString());
+        orderDto.setTotalPrice(orderDto.getUnitPrice() * orderDto.getQty() );
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT); // 완전히 동일하게 매핑되어야 매핑됨
+        OrderEntity orderEntity = mapper.map(orderDto, OrderEntity.class); // userDto를 UserEntity로 매핑
+
+        orderRepository.save(orderEntity);
+
+        OrderDto returnOrderDto = mapper.map(orderEntity, OrderDto.class);
+        return returnOrderDto;
+    }
+
+
+    @Override
+    public OrderDto getOrderByOrderId(String orderId) {
+        OrderEntity orderEntity = orderRepository.findByOrderId(orderId);
+        OrderDto orderDto = new ModelMapper().map(orderEntity, OrderDto.class);
+        return orderDto;
+    }
+
+    @Override
+    public Iterable<OrderEntity> getOrdersByUserId(String userId) {
+        return orderRepository.findByUserId(userId);
+    }
+}
