ab19628db1290e457c6f97f750986414110e33bb
suyong <emuext@gmail.com>
2023-08-15 14:16:19 +0900

    kafka 설정

--- src/main/java/com/example/orderservice/controller/OrderController.java
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.example.orderservice.controller;
 
 import com.example.orderservice.dto.OrderDto;
 import com.example.orderservice.jpa.OrderEntity;
+import com.example.orderservice.messagequeue.KafkaProducer;
+import com.example.orderservice.messagequeue.OrderProducer;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +17,7 @@ import com.example.orderservice.vo.ResponseOrder;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/order-service")
@@ -23,10 +26,16 @@ public class OrderController {
     Environment env;
     OrderService orderService;
 
+    KafkaProducer kafkaProducer;
+
+    OrderProducer orderProducer;
+
     @Autowired
-    public OrderController(Environment env, OrderService orderService) {
+    public OrderController(Environment env, OrderService orderService, KafkaProducer kafkaProducer, OrderProducer orderProducer) {
         this.env = env;
         this.orderService = orderService;
+        this.kafkaProducer = kafkaProducer;
+        this.orderProducer = orderProducer;
     }
 
 
@@ -42,10 +51,18 @@ public class OrderController {
 
         OrderDto orderDto = mapper.map(orderDetails, OrderDto.class);
         orderDto.setUserId(userId);
+        //jpa 사용부분 주석처리
+//        OrderDto createdOrder = orderService.createOrder(orderDto);
+//        ResponseOrder responseOrder = mapper.map(createdOrder, ResponseOrder.class);
 
-        OrderDto createdOrder = orderService.createOrder(orderDto);
+        //jpa대신 kafka로 변경
+        orderDto.setOrderId(UUID.randomUUID().toString());
+        orderDto.setTotalPrice(orderDetails.getQty() * orderDetails.getUnitPrice());
 
-        ResponseOrder responseOrder = mapper.map(createdOrder, ResponseOrder.class);
+        //kafka에 주문정보 전달, send this order to kafka
+        kafkaProducer.send("example-catalog-topic", orderDto); // catalog-service에 컨슈머에 등록한 값가 동일한 값으로 설정
+        orderProducer.send("orders", orderDto);
+        ResponseOrder responseOrder = mapper.map(orderDto, ResponseOrder.class);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(responseOrder);
     }
