aa51528545314d866b6a737887d4ed4fcf3feeef
suyong <emuext@gmail.com>
2023-08-15 16:15:12 +0900

    zipkin과 sleuth 설정 추가

--- src/main/java/com/example/orderservice/controller/OrderController.java
+++ src/main/java/com/example/orderservice/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.example.orderservice.dto.OrderDto;
 import com.example.orderservice.jpa.OrderEntity;
 import com.example.orderservice.messagequeue.KafkaProducer;
 import com.example.orderservice.messagequeue.OrderProducer;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("/order-service")
+@Slf4j
 public class OrderController {
 
     Environment env;
@@ -46,35 +48,49 @@ public class OrderController {
 
     @PostMapping("/{userId}/orders")
     public ResponseEntity<ResponseOrder> createOrder(@PathVariable("userId") String userId, @RequestBody RequestOrder orderDetails){
+        log.info("Before insert order data");
+
         ModelMapper mapper = new ModelMapper();
         mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
 
         OrderDto orderDto = mapper.map(orderDetails, OrderDto.class);
         orderDto.setUserId(userId);
         //jpa 사용부분 주석처리
-//        OrderDto createdOrder = orderService.createOrder(orderDto);
-//        ResponseOrder responseOrder = mapper.map(createdOrder, ResponseOrder.class);
+        OrderDto createdOrder = orderService.createOrder(orderDto);
+        ResponseOrder responseOrder = mapper.map(createdOrder, ResponseOrder.class);
 
         //jpa대신 kafka로 변경
-        orderDto.setOrderId(UUID.randomUUID().toString());
-        orderDto.setTotalPrice(orderDetails.getQty() * orderDetails.getUnitPrice());
+//        orderDto.setOrderId(UUID.randomUUID().toString());
+//        orderDto.setTotalPrice(orderDetails.getQty() * orderDetails.getUnitPrice());
 
         //kafka에 주문정보 전달, send this order to kafka
-        kafkaProducer.send("example-catalog-topic", orderDto); // catalog-service에 컨슈머에 등록한 값가 동일한 값으로 설정
-        orderProducer.send("orders", orderDto);
-        ResponseOrder responseOrder = mapper.map(orderDto, ResponseOrder.class);
+//        kafkaProducer.send("example-catalog-topic", orderDto); // catalog-service에 컨슈머에 등록한 값가 동일한 값으로 설정
+//        orderProducer.send("orders", orderDto);
+//        ResponseOrder responseOrder = mapper.map(orderDto, ResponseOrder.class);
+        log.info("After insert order data");
 
         return ResponseEntity.status(HttpStatus.CREATED).body(responseOrder);
     }
 
     @GetMapping("/{userId}/orders")
-    public ResponseEntity<List<ResponseOrder>> createOrder(@PathVariable("userId") String userId){
+    public ResponseEntity<List<ResponseOrder>> createOrder(@PathVariable("userId") String userId) throws Exception{
+        log.info("Before received order data");
+
         Iterable<OrderEntity> orderList = orderService.getOrdersByUserId(userId);
         List<ResponseOrder> result = new ArrayList();
 
         orderList.forEach(v -> {
             result.add(new ModelMapper().map(v, ResponseOrder.class));
         });
+
+        try{
+            Thread.sleep(1000);
+            throw new Exception("장애발생");
+        }catch (InterruptedException e){
+            log.warn(e.getMessage());
+        }
+
+        log.info("After received order data");
 
         return ResponseEntity.status(HttpStatus.OK).body(result);
     }
